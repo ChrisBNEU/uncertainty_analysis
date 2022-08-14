704e7985660c35ebda2b88d166110fd7fc600d8c
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2022-08-14 19:02:31 -0400

    adding global uncertainty folder

---
+++ rmg_gua/global/create_uqtk_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=gsa_input
+#SBATCH --time=01:00:00
+#SBATCH --error=uq_error.log
+#SBATCH --output=uq_output.log
+#SBATCH --nodes=1
+#SBATCH --partition=short
+#SBATCH --mem=20Gb
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=1
+#SBATCH --mail-user=$USER@northeastern.edu 
+#SBATCH --mail-type=FAIL,END
+
+# load in the initialization script
+source activate /work/westgroup/ChrisB/_01_MeOH_repos/uncertainty_analysis/conda
+python-jl create_uqtk_files.py
+
---
+++ rmg_gua/global/run_workflow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=uqtk
+#SBATCH --time=1:00:00
+#SBATCH --error=uq_error.log
+#SBATCH --output=uq_output.log
+#SBATCH --nodes=1
+#SBATCH --mem=100Gb
+#SBATCH --partition=short
+#SBATCH --ntasks=1
+#SBATCH --mail-user=$USER@northeastern.edu 
+#SBATCH --mail-type=FAIL,END
+
+source activate uqtk
+source activate_uqtk.sh 
+sh workflow_meoh.x
