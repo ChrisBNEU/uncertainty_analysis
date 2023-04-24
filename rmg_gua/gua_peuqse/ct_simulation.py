ae7e5e8d7d0db6ee048c715c82785bf80ebfde0d
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2023-04-24 15:17:44 -0400

    made ct sim wrapper run faster

--- rmg_gua/gua_peuqse/ct_simulation.py
+++ rmg_gua/gua_peuqse/ct_simulation.py
@@ -10,6 +10,7 @@ repo_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 sys.path.append(repo_dir)
 
 from rmg_gua.gua_cantera.Spinning_basket_reactor.sbr import MinSBR
+from rmg_gua.gua_peuqse.runtime_utilities import get_all_param_lists
 from cantera import CanteraError
 from PEUQSE import parallel_processing
 
@@ -18,6 +19,7 @@ def sim_init(project_path):
     global peuq_path
     global data
     global lookup_dict
+    global test_sbr
 
     results_path = os.path.join(project_path, "config")
     peuq_path = os.path.join(project_path, "peuqse")
@@ -25,11 +27,26 @@ def sim_init(project_path):
     expt_condts = os.path.join(results_path, "expt_data_orig.yaml")
     lookup_dict_file = os.path.join(results_path, "rmg_2_ck_dict.yaml")
 
+    file_path = os.path.join(repo_dir, "rmg_gua", "baseline", "cantera", "chem_annotated.cti")
+
     # load the exp data and lookup dict
     with open(expt_condts, 'r') as f:
         data = yaml.load(f, Loader = yaml.FullLoader)
     with open(lookup_dict_file, 'r') as f:
         lookup_dict = yaml.load(f, Loader = yaml.FullLoader)
+    
+    # load the initial parameter set
+    starting_params = get_all_param_lists(results_path)
+
+    # initialize sbr
+    test_sbr = MinSBR(
+        file_path,
+        reac_config=data[0],
+        rtol=1.0e-11,
+        atol=1.0e-22,
+        reaction_list=starting_params["value_list"], 
+        results_path=results_path,
+    )
 
 
 # To use PEUQSE, you can have a function, but you also need to make a function wrapper that takes *only* the parameters as a single vector.
@@ -39,17 +56,12 @@ def simulationFunction(parameters, debug=False):
     """
     run rms reactor. 
     """
-    # build and run the simulation
-    file_path = os.path.join(repo_dir, "rmg_gua", "baseline", "cantera", "chem_annotated.cti")
     
-
     CH3OH_X = []
     CO_X = []
     CO2_X = []
     H2_X = []
     H2O_X = []
-    # change the rms file. now doing all reactions in mechanism
-
 
     # pick just one experiment for example. can in the future use multiprocessing to solve faster, 
     # for now just do in series. 
@@ -57,23 +69,32 @@ def simulationFunction(parameters, debug=False):
     run_test = False
     n_expts = len(data)
             
-    print(f"length is {n_expts} in sim")
+    # print(f"length is {n_expts} in sim")
+    
     for run in range(0,n_expts): 
+    # for run in [0]:
         conditions = data[run]
-        print(f"starting sim {run}")
+        # print(f"starting sim {run}")
         
         # we modify rates in memory
-        test_sbr = MinSBR(
-            file_path,
-            reac_config=conditions,
-            rtol=1.0e-11,
-            atol=1.0e-22,
-            reaction_list=parameters, 
-            results_path=results_path,
-        )
+        if run == 0:
+            test_sbr.set_params(
+                reac_config=conditions,
+                rtol=1.0e-11,
+                atol=1.0e-22,
+                reaction_list=parameters
+            )
+        else:
+            # print("resetting reactor")
+            test_sbr.reset_reactor(
+                reac_config=conditions,
+                rtol=1.0e-11,
+                atol=1.0e-22,
+            )
+
         # try except loop for this because cantera errors out for certain A and Ea values
         try:
-            results = test_sbr.run_reactor_ss_memory()
+            results = test_sbr.run_reactor_ss_memory(peuqse=True)
             CH3OH_X.append(results[lookup_dict["CH3OH"]])
             CO_X.append(results[lookup_dict["CO"]])
             CO2_X.append(results[lookup_dict["CO2"]])
@@ -90,43 +111,39 @@ def simulationFunction(parameters, debug=False):
             H2O_X.append(float('nan'))
 
     y_data = np.vstack([CH3OH_X, CO_X, CO2_X, H2_X, H2O_X])
-    pnum = parallel_processing.currentProcessorNumber
+    # pnum = parallel_processing.currentProcessorNumber
     
     # is there a faster filetype? json?
-    peuq_output_yaml_path = os.path.join(peuq_path, f"{pnum}_out.yaml")
-    if os.path.exists(peuq_output_yaml_path):
-        with open(peuq_output_yaml_path, "r") as f:
-            peuq_output_yaml = yaml.safe_load(f)
-    else: 
-        peuq_output_yaml = []
+    # peuq_output_yaml_path = os.path.join(peuq_path, f"{pnum}_out.yaml")
+    # if os.path.exists(peuq_output_yaml_path):
+    #     with open(peuq_output_yaml_path, "r") as f:
+    #         peuq_output_yaml = yaml.safe_load(f)
+    # else: 
+    #     peuq_output_yaml = []
         
     # not sure why but peuqse has an array for first iteration and a tuple for
     # successive ones for input parameters
     # print("dtype output: ", type(parameters[0]), type(y_data[0][0]))
-    if isinstance(parameters, tuple):
-        parameters = np.array(parameters).astype(float).tolist()
-        y_data_list = y_data.astype(float).tolist()
-    else: 
-        parameters = parameters.astype(float).tolist()
-        y_data_list = y_data.astype(float).tolist()
+    # if isinstance(parameters, tuple):
+    #     parameters = np.array(parameters).astype(float).tolist()
+    #     y_data_list = y_data.astype(float).tolist()
+    # else: 
+    #     parameters = parameters.astype(float).tolist()
+    #     y_data_list = y_data.astype(float).tolist()
         
     
-    peuq_output_yaml.append((parameters, y_data_list))
+    # peuq_output_yaml.append((parameters, y_data_list))
     
-    with open(peuq_output_yaml_path, "w") as f:
-        yaml.safe_dump(peuq_output_yaml, f)
+    # with open(peuq_output_yaml_path, "w") as f:
+    #     yaml.safe_dump(peuq_output_yaml, f)
     
-    print("Results:\n")
-    print("CH3OH: ", CH3OH_X)
-    print("CO: ", CO_X)
-    print("CO2: ", CO2_X)
-    print("H2: ", H2_X)
-    print("H2O: ", H2O_X)
-    print("sim done")
-    t2 = time.asctime()
-    t2s = time.time()
-    
-
+    # print("Results:\n")
+    # print("CH3OH: ", CH3OH_X)
+    # print("CO: ", CO_X)
+    # print("CO2: ", CO2_X)
+    # print("H2: ", H2_X)
+    # print("H2O: ", H2O_X)
+    # print("sim done")
     
     if debug:
         return test_sbr, y_data
@@ -139,7 +156,7 @@ def simulation_function_wrapper(parametersArray):#this has a and b in it.
     t1 = time.asctime()
     t1s = time.time()
 
-    y = simulationFunction(parametersArray)  #an alternate simpler syntax to unpack the parameters would be: simulationFunction(x_values_for_data, *parametersArray) 
+    y = simulationFunction(parametersArray) 
     
     t2 = time.asctime()
     t2s = time.time()
