5e069bfe07f4d61cfd190cacf7e02fa628319472
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2023-04-27 14:15:14 -0400

    cleaned up ct wrapper, and updated for sbr change

--- rmg_gua/gua_peuqse/ct_simulation.py
+++ rmg_gua/gua_peuqse/ct_simulation.py
@@ -5,7 +5,7 @@ import os
 import logging
 import random
 import time
-import h5py
+import cProfile
 repo_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(repo_dir)
 
@@ -57,6 +57,8 @@ def sim_init(project_path):
         atol=1.0e-22,
         reaction_list=starting_params["value_list"], 
         results_path=results_path,
+        use_precond=False, 
+        time=600,
     )
 
 
@@ -80,27 +82,23 @@ def simulationFunction(parameters, debug=False):
     run_test = False
     n_expts = len(data)
             
-    # print(f"length is {n_expts} in sim")
-    
+    file_path = os.path.join(repo_dir, "rmg_gua", "baseline", "cantera", "chem_annotated.yaml")
+    test_sbr.reset_params(        
+        reac_config=data[0], 
+        rtol=1.0e-11,
+        atol=1.0e-22,
+        reaction_list=parameters,
+        )
+
     for run in range(0,n_expts): 
-    # for run in [0]:
         conditions = data[run]
         # print(f"starting sim {run}")
         
-        # we modify rates in memory
-        if run == 0:
-            test_sbr.set_params(
-                reac_config=conditions,
-                rtol=1.0e-11,
-                atol=1.0e-22,
-                reaction_list=parameters
-            )
-        else:
-            # print("resetting reactor")
-            test_sbr.reset_reactor(
-                reac_config=conditions,
-                rtol=1.0e-11,
-                atol=1.0e-22,
+        # print("resetting reactor")
+        test_sbr.reset_reactor(
+            reac_config=conditions,
+            rtol=1.0e-11,
+            atol=1.0e-22,
             )
 
         # try except loop for this because cantera errors out for certain A and Ea values
@@ -122,42 +120,42 @@ def simulationFunction(parameters, debug=False):
             H2O_X.append(float('nan'))
 
     y_data = np.vstack([CH3OH_X, CO_X, CO2_X, H2_X, H2O_X])
-    
-    # pnum = parallel_processing.currentProcessorNumber
-    
-    # is there a faster filetype? json?
-    # peuq_output_yaml_path = os.path.join(peuq_path, f"{pnum}_out.yaml")
-    # if os.path.exists(peuq_output_yaml_path):
-    #     with open(peuq_output_yaml_path, "r") as f:
-    #         peuq_output_yaml = yaml.safe_load(f)
-    # else: 
-    #     peuq_output_yaml = []
+
+    if debug: 
+        # save processor number if we are using mpi
+        pnum = parallel_processing.currentProcessorNumber
+
+        # save a yaml file of the output (per processor if mpi)
+        peuq_output_yaml_path = os.path.join(peuq_path, f"{pnum}_out.yaml")
+        if os.path.exists(peuq_output_yaml_path):
+            with open(peuq_output_yaml_path, "r") as f:
+                peuq_output_yaml = yaml.safe_load(f)
+        else: 
+            peuq_output_yaml = []
+            
+        # not sure why but peuqse has an array for first iteration and a tuple for
+        # successive ones for input parameters, so we need to check datatype
+        print("dtype output: ", type(parameters[0]), type(y_data[0][0]))
+        if isinstance(parameters, tuple):
+            parameters = np.array(parameters).astype(float).tolist()
+            y_data_list = y_data.astype(float).tolist()
+        else: 
+            parameters = parameters.astype(float).tolist()
+            y_data_list = y_data.astype(float).tolist()
+            
         
-    # not sure why but peuqse has an array for first iteration and a tuple for
-    # successive ones for input parameters
-    # print("dtype output: ", type(parameters[0]), type(y_data[0][0]))
-    # if isinstance(parameters, tuple):
-    #     parameters = np.array(parameters).astype(float).tolist()
-    #     y_data_list = y_data.astype(float).tolist()
-    # else: 
-    #     parameters = parameters.astype(float).tolist()
-    #     y_data_list = y_data.astype(float).tolist()
+        peuq_output_yaml.append((parameters, y_data_list))
         
-    
-    # peuq_output_yaml.append((parameters, y_data_list))
-    
-    # with open(peuq_output_yaml_path, "w") as f:
-    #     yaml.safe_dump(peuq_output_yaml, f)
-    
-    # print("Results:\n")
-    # print("CH3OH: ", CH3OH_X)
-    # print("CO: ", CO_X)
-    # print("CO2: ", CO2_X)
-    # print("H2: ", H2_X)
-    # print("H2O: ", H2O_X)
-    # print("sim done")
-    
-    if debug:
+        with open(peuq_output_yaml_path, "w") as f:
+            yaml.safe_dump(peuq_output_yaml, f)
+        
+        print("Results:\n")
+        print("CH3OH: ", CH3OH_X)
+        print("CO: ", CO_X)
+        print("CO2: ", CO2_X)
+        print("H2: ", H2_X)
+        print("H2O: ", H2O_X)
+        print("sim done")
         return test_sbr, y_data
     else:
         return y_data 
